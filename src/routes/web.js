f9aeb08f6eaa2dd3a3809be7f64f6084694d00d8
Khanh Hoang <nguyenlehoangkhanh1679@gmail.com>
2022-11-19 19:17:30 +0700

    19-11-2022

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import homeController from '../controllers/homeController.js';
 import userController from '../controllers/userController.js';
+import doctorController from '../controllers/doctorController.js'
 
 
 const router = express.Router();
@@ -23,7 +24,10 @@ const initWebRoutes = (app) => {
     router.post('/api/create-new-user', userController.handleCreateNewUser);
     router.put('/api/edit-user', userController.handleEditUser);
     router.delete('/api/delete-user', userController.handleDeleteUser);
-    router.get('/api/allcode', userController.getAllCode)
+    router.get('/api/allcode', userController.getAllCode);
+
+
+    router.get('/api/top-doctor-home', doctorController.getTopDoctorHome);
     
     return app.use('/', router);
 }
