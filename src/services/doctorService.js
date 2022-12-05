63e3b9ee18314f3c40a6aae1e528c2fe032c4b61
Khanh Hoang <nguyenlehoangkhanh1679@gmail.com>
2022-12-05 19:01:54 +0700

    #92

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -58,18 +58,35 @@ const handleGetAllDoctors = () => {
     })
 }
 
+const checkRequiredFields = (inputData) => {
+    let arrFields = ['doctorId', 'contentHTML', 'contentMarkdown', 
+                'action', 'selectedPrice', 'selectedPayment', 
+                'selectedProvince', 'nameClinic', 'addressClinic', 'note', 'specialtyId'];
+
+    let isValid = true;
+    let element = "";
+
+    for(let i = 0; i < arrFields.length; i++){
+        if(!inputData[arrFields[i]]){
+            isValid = false;
+            element = arrFields[i];
+            break;
+        }
+    }
+
+    return {isValid, element}
+}
+
 const handleSaveDetailInfoDoctor = (inputData) => {
     return new Promise(async (resolve, reject) => {
         try {
 
-            if(
-                !inputData.doctorId || !inputData.contentHTML || !inputData.contentMarkdown  || !inputData.action
-                || !inputData.selectedPrice || !inputData.selectedPayment || !inputData.selectedProvince
-                || !inputData.nameClinic || !inputData.addressClinic || !inputData.note
-            ){
+            const checkObj = checkRequiredFields(inputData);
+
+            if(!checkObj.isValid){
                 resolve({
                     errCode: 1,
-                    errMessage: 'Missing parameter'
+                    errMessage: `Missing parameter: ${checkObj.element}`
                 })
     
             }else{
@@ -113,6 +130,9 @@ const handleSaveDetailInfoDoctor = (inputData) => {
                     doctorInfor.nameClinic = inputData.nameClinic;
                     doctorInfor.addressClinic = inputData.addressClinic;
                     doctorInfor.note = inputData.note;
+                    doctorInfor.specialtyId = inputData.specialtyId;
+                    doctorInfor.clinicId = inputData.clinicId;
+
                     
                     await doctorInfor.save();
 
@@ -126,6 +146,8 @@ const handleSaveDetailInfoDoctor = (inputData) => {
                         nameClinic: inputData.nameClinic,
                         addressClinic: inputData.addressClinic,
                         note: inputData.note,
+                        specialtyId: inputData.specialtyId,
+                        clinicId: inputData.clinicId
                     })
                 }
             }
